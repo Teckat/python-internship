6ceb07da6e1823334a664afc86718e0ec1bf6c6f
pritam kundu <pritamkundu60.com>
2020-07-06 17:09:43 +0530

    session 1 day 2 completed

---
+++ session1/day2.py
@@ -0,0 +1,95 @@
+
+
+# print("don't disobey")
+
+# ========================================  data types start======================================================
+
+#  integer
+
+
+# x = 10
+# y = 20000000000000000000555555555555555555555555555555555555555555555555554444444444444444444444444444444444444
+# print(type(x), x, y)
+
+
+# #  float
+
+# f = 10.20
+# print(type(f), f)
+
+# # string
+
+# s = "teckat"
+# print(type(s), s)
+
+
+# # complex
+
+# c = 10+2j
+# d = 20+30j
+# e = 4+3j
+# print(type(c), c, d)
+
+# # addition of complex
+# print("sum = ", c+d)
+
+# #  complex fuction
+# z = complex(4, -9)
+# print("using complex function", z)
+
+# # separting real and imaginary parts
+
+# print("real part = ", c.real)
+# print("imaginary part = ", c.imag)
+
+# #  print the absolute value
+
+# print("absolute value method 1", abs(e))
+
+# print("absolute value method 2", abs(complex(4, -3)))
+
+
+# ========================================  data types end======================================================
+
+
+# ========================================  type cast start======================================================
+
+x = 2.5
+print(type(x), x)
+
+# int type cast
+y = int(2.5)
+print("type cast int =", type(y), y)
+
+# float type cast
+z = 5
+print(type(z), z)
+
+f = float(5)
+print("type cast float", type(f), f)
+
+# string type cast
+
+a = 20
+print(type(a), a, a+20)
+
+s = str(20)
+print("type cast string", type(s), s, s)
+
+# divmod function
+print(divmod(9, 2))
+
+# ========================================  type cast end  ======================================================
+
+#  how to visualize sequence
+#  string
+x = "teckat"
+
+#  list
+l = [1, 2]
+
+#  dictionary
+d = {'key': 'value'}
+
+# tuple
+teckat = (1, 2)
