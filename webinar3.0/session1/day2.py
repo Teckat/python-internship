5cd36b178fdc96714835bdc96b56f42059cf2be2
pritam kundu <pritamkundu60.com>
2020-09-09 20:51:23 +0530

    webinar 3.0 session 1 day 2 completed

---
+++ webinar3.0/session1/day2.py
@@ -0,0 +1,79 @@
+
+# data types
+
+# 1. integer
+# a = 125463122541454545454515121544445451213212121212121545432121511321353213134532132154545454156445454145454515454545454544545454545454545464645656545664124
+# print(a)
+
+# print(type(a))
+
+
+# 2. Float
+
+# x = 2.4213412424241242
+# print(x)
+# print(type(x))
+
+
+# 3. complex numbers
+
+# a = 3+5j
+# b = 4
+# c = 10j
+# print(a, b, c)
+# print(b)
+# print(c)
+
+# print(a, '\n', b, '\n', c)
+
+# z = complex(2, 3)
+# print(z)
+
+# print(complex(2, 5))
+
+# print(a.real, a.imag)
+
+# find absolute value (
+# a=x+yj
+# |a|= sqrt(x2 + y2)
+# )
+
+# print(abs(complex(3, 4)))
+
+
+#  type casting
+
+# integer type casting
+
+# z = "45"
+
+# print(type(z))
+
+# f = 4.65
+# print(type(f))
+
+# a = int(f)
+# print(a)
+# print(type(a))
+
+
+#  float type
+
+# x = '34.89'
+# a = float(x)
+# print(a, type(a))
+
+
+# divmod
+
+# (x, y) = divmod(21, 8)
+# print(x, y)
+
+# print(divmod(4, 3))
+
+x = 10
+print(x)
+y = 20
+print(x, y)
+
+a1 = 21
