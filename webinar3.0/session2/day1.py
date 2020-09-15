922a287ea5e71d15dacf0f6b46ea1ab5b24d7ce0
pritam kundu <pritamkundu60.com>
2020-09-15 20:45:32 +0530

    session 2 day 1

---
+++ webinar3.0/session2/day1.py
@@ -0,0 +1,74 @@
+#  tuple
+
+# x = (12, 3, 4, 556, 77, 3, 32, 3, 334)
+
+# print(x, type(x))
+
+
+#  indexing
+
+# print(x[0])
+# print(x[6])
+# print(x[-1])
+
+# slicing
+
+# a = x[2:5]
+
+# print(a)
+
+
+#  delete
+
+# del x
+# print(x)
+
+# count
+
+# print(x.count(3))
+
+
+# dictionary
+
+x = {'a': [12, 13, 14], 'b': {'name': 'teckat', 'class': 'entrepreneur'}, 'c': 3214,
+     'd': 455, 'e': 466, 'f': 212, 'g': 232}
+
+
+# print(x, type(x))
+
+# print(x['b'])
+
+# find keys()
+
+# print(x.keys())
+
+# find values()
+# print(x.values())
+
+# adding data
+
+# x['h'] = 'abcd'
+# x['g'] = 100
+# print(x)
+
+#  remove
+
+# x.pop('a')
+# print(x)
+
+
+#  update
+# u = {'a': 2304, 'h': 123}
+# x.update(u)
+
+# print(x)
+
+# delete the entire dictionary
+
+# del x
+# print(x)
+
+# clear
+
+x.clear()
+print(x)
